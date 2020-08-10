98b6672c49425e92521218def6bdd1837360cf28
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-08-10 18:40:34 +0800

    feature: 增加错误码

---
+++ blog/errcode.py
@@ -0,0 +1,31 @@
+#####################################
+SUCCESS = {
+    "err_code": 0,
+    "data": {},
+    "detail": " SUCCESS "
+}
+
+PARAM_ERROR = {
+    "err_code": -1,
+    "data": {},
+    "detail": " PARAM ERROR "
+}
+
+ABNORMAL_BEHAVIOR = {
+    "err_code": -2,
+    "data": {},
+    "detail": " WARNING ! ABNORMAL BEHAVIOR !"
+}
+
+# 用户相关
+LOG_SUCCESS = {
+    "err_code": 1001,
+    "data": {},
+    "detail": "log in success"
+}
+
+LOG_FAIL = {
+    "err_code": 1002,
+    "data": {},
+    "detail": "log in fail"
+}
