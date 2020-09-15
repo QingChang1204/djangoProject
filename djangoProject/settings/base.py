8843a757881193305cfe9d21ea161123eba3b73d
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-15 18:24:24 +0800

    feat: 修正https URL

--- djangoProject/settings/base.py
+++ djangoProject/settings/base.py
@@ -173,6 +173,7 @@ USE_L10N = True
 
 USE_TZ = False
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
