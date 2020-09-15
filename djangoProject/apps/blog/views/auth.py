282deeee2c19351d848e53c4ecec7852a37d1100
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-15 16:45:32 +0800

    fix: 修正username password不可为None

--- djangoProject/apps/blog/views/auth.py
+++ djangoProject/apps/blog/views/auth.py
@@ -6,6 +6,9 @@ from blog.models import User
 class CustomBackend(ModelBackend):
 
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return None
+
         try:
             user = User.objects.get(
                 Q(username=username) |
