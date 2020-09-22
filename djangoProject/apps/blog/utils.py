b6ab4056c22a35edc903c8450efdd8b4ce100778
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-22 17:53:38 +0800

    feat: 修复redis输出bytes

--- djangoProject/apps/blog/utils.py
+++ djangoProject/apps/blog/utils.py
@@ -1,6 +1,13 @@
 import logging
-
+from django_redis.pool import ConnectionFactory
 from blog.search import SearchByEs
 
 logger = logging.getLogger(__name__)
 search = SearchByEs()
+
+
+class DecodeConnectionFactory(ConnectionFactory):
+
+    def get_connection(self, params):
+        params['decode_responses'] = True
+        return super().get_connection(params)
