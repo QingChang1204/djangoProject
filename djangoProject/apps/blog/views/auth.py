32141c499e30409a10860de4587ef6d38cd58b63
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-23 16:57:57 +0800

    feat: 验证未通过直接返回错误信息

--- djangoProject/apps/blog/views/auth.py
+++ djangoProject/apps/blog/views/auth.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from rest_framework import exceptions
 from blog.models import User
 
 
@@ -7,7 +8,9 @@ class CustomBackend(ModelBackend):
 
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None or password is None:
-            return None
+            raise exceptions.AuthenticationFailed(
+                "用户名或者密码不可为空。"
+            )
 
         try:
             user = User.objects.get(
@@ -16,5 +19,11 @@ class CustomBackend(ModelBackend):
             )
             if user.check_password(password):
                 return user
+            else:
+                raise exceptions.AuthenticationFailed(
+                    "密码不正确,请重新输入。"
+                )
         except User.DoesNotExist:
-            return None
+            raise exceptions.AuthenticationFailed(
+                "账号不存在,请核对账号后重新输入。"
+            )
