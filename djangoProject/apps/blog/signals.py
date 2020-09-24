30064185e042d93733c19d8de7188fc7871d5415
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-24 13:04:31 +0800

    feat: 通过signals设置文章搜索内容

---
+++ djangoProject/apps/blog/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from blog.models import Article
+from blog.utils import search
+
+
+@receiver(post_save, sender=Article)
+def search_article(instance, **kwargs):
+    search_word = instance.content + instance.title
+    if instance.tag is not None:
+        search_word += instance.tag
+    search.handle_search(instance.id, search_word, instance.publish_status)
