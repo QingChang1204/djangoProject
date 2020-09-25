cff8dcb21a4ccba4295285841e3c1c8ff8189964
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-25 13:17:32 +0800

    feat: 增加开发的 allow host

--- djangoProject/settings/dev.py
+++ djangoProject/settings/dev.py
@@ -10,6 +10,6 @@ else:
     CELERY_BROKER_URL = BROKER_URL_DEV
     CELERY_RESULT_BACKEND = CELERY_RESULT_BACKEND_DEV
 
-
+ALLOWED_HOSTS = ['*']
 # Separate Migrations
 MIGRATION_MODULES = {app: '%s.dev_migrations' % app for app in MIGRATE_APPS}
