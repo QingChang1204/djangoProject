a2f3f9659653064cae4f12a35ed8cd105bc153d5
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-25 16:21:03 +0800

    feat: 增加主界面404

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path, include
 
+
+def index(request):
+    return render(request, '404.html')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('blog.urls'))
+    path('api/', include('blog.urls')),
+    path('', index)
 ]
