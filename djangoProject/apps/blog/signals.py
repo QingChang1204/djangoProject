ae2986593a01406bba696fbe4dcde50787123da9
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-25 15:12:37 +0800

    feat: 优化代码结构(signal, search)

--- djangoProject/apps/blog/signals.py
+++ djangoProject/apps/blog/signals.py
@@ -1,15 +1,28 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_delete, post_save
 from django.dispatch import receiver
 from blog.models import Article, Comment, Reply, AttachedPicture
 from blog.utils import search
 
 
 @receiver(post_save, sender=Article)
-def search_article(instance, **kwargs):
-    search_word = instance.content + instance.title
-    if instance.tag is not None:
-        search_word += instance.tag
-    search.handle_search(instance.id, search_word, instance.publish_status)
+def post_search_article(instance, created, **kwargs):
+    if created:
+        search_word = instance.content + instance.title
+        if instance.tag is not None:
+            search_word += instance.tag
+        search.handle_search(instance.id, search_word, instance.publish_status)
+
+
+@receiver(pre_save, sender=Article)
+def put_search_article(instance, update_fields, **kwargs):
+    check = ['content', 'title', 'tag']
+    if instance.id is None:
+        pass
+    elif any([info in update_fields for info in check]):
+        search_word = instance.content + instance.title
+        if instance.tag is not None:
+            search_word += instance.tag
+        search.handle_search(instance.id, search_word, instance.publish_status)
 
 
 @receiver(post_delete, sender=Article)
