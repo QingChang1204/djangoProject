7e92b87765766b40398b62ec6c1de4cdd6434bb9
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-09-27 16:49:00 +0800

    feat: 异步创建图片，提升响应速度

--- djangoProject/apps/blog/tasks.py
+++ djangoProject/apps/blog/tasks.py
@@ -1,8 +1,54 @@
 from __future__ import absolute_import, unicode_literals
+from rest_framework import serializers
 from blog.utils import search, logger
 from celery.decorators import task, periodic_task
 from celery.task.schedules import crontab
 from blog.models import AttachedPicture, Reply
+
+
+class AttachedPictureSerializers(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False, allow_null=True)
+
+    def __init__(self, *args, **kwargs):
+        self.old_instance_id_list = []
+        self.new_instance_id_list = []
+        self.__class__.old_instance_id_list = self.old_instance_id_list
+        self.attached_table = kwargs.pop('attached_table', None)
+        self.attached_id = kwargs.pop('attached_id', None)
+        super(AttachedPictureSerializers, self).__init__(*args, **kwargs)
+
+    def get_old_instance_id_list(self):
+        return self.old_instance_id_list
+
+    class Meta:
+        model = AttachedPicture
+        fields = [
+            'image', 'id'
+        ]
+
+    def create(self, validated_data):
+        pic_id = validated_data.pop('id', None)
+        if pic_id is not None:
+            self.old_instance_id_list.append(pic_id)
+            pass
+        else:
+            instance = self.Meta.model(
+                **validated_data,
+                attached_id=self.attached_id,
+                attached_table=self.attached_table
+            )
+            instance.save()
+            self.new_instance_id_list.append(instance.id)
+            return instance
+
+    def remove_old_instance(self):
+        self.Meta.model.objects.stealth_delete(
+            attached_id=self.attached_id,
+            attached_table=self.attached_table,
+            old_id_list=self.old_instance_id_list + self.new_instance_id_list
+        )
+        self.old_instance_id_list = []
+        self.new_instance_id_list = []
 
 
 @task
@@ -32,6 +78,20 @@ def delete_reply(instance_id):
 @task
 def synchronous_username(old_username, new_username):
     search.update_search_by_author(old_username, new_username)
+
+
+@task
+def set_attached_picture(images, attached_table, attached_id):
+    image_serializers = AttachedPictureSerializers(
+        data=images,
+        many=True,
+        attached_table=attached_table,
+        attached_id=attached_id
+    )
+    image_serializers.is_valid(raise_exception=True)
+    image_serializers.save()
+    if image_serializers.child.get_old_instance_id_list:
+        image_serializers.child.remove_old_instance()
 
 
 @periodic_task(run_every=(crontab(minute=1, hour=0)), ignore_result=True)
