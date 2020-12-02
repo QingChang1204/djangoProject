db2ddaf1eccc962cd9a8a9cae6327caba38f0128
QingChang <49056266+LIuQingChang@users.noreply.github.com>
2020-12-02 19:01:44 +0800

    feat: 升级celery=>5

--- djangoProject/apps/blog/tasks.py
+++ djangoProject/apps/blog/tasks.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
+from celery import shared_task
 from celery.schedules import crontab
 from django.db.models import Q
 from rest_framework import serializers
 from djangoProject.celery import app as current_app
-from celery.task import Task, PeriodicTask
 from blog.models import Reply, ArticleImages
 from blog.utils import es_search, logger, robot_send_alert
 
@@ -19,7 +19,7 @@ class ArticleImagesSerializers(serializers.ModelSerializer):
         ]
 
 
-@current_app.task(name='blog_signal.search_article', base=Task)
+@current_app.task(name='blog_signal.search_article')
 def search_article(article_id, content, title, tag, publish_status, author):
     """
     设置文章搜索词
@@ -37,7 +37,7 @@ def search_article(article_id, content, title, tag, publish_status, author):
     es_search.handle_search(article_id, search_word, publish_status, author)
 
 
-@current_app.task(name='blog_signal.delete_attached_picture', base=Task)
+@current_app.task(name='blog_signal.delete_attached_picture')
 def delete_attached_picture(attached_table, attached_id):
     """
     移除关联图片，关联词
@@ -59,7 +59,7 @@ def delete_attached_picture(attached_table, attached_id):
         pass
 
 
-@current_app.task(name='blog_signal.delete_reply', base=Task)
+@current_app.task(name='blog_signal.delete_reply')
 def delete_reply(instance_id):
     """
     移除关联回复
@@ -71,7 +71,7 @@ def delete_reply(instance_id):
     ).delete()
 
 
-@current_app.task(name='blog_signal.synchronous_username', base=Task)
+@current_app.task(name='blog_signal.synchronous_username')
 def synchronous_username(old_username, new_username):
     """
     搜索引擎搜索内容同步用户名修改
@@ -82,7 +82,7 @@ def synchronous_username(old_username, new_username):
     es_search.update_search_by_author(old_username, new_username)
 
 
-@current_app.task(name='blog_signal.set_attached_picture', base=Task)
+@current_app.task(name='blog_signal.set_attached_picture')
 def set_attached_picture(images, attached_table, attached_id):
     """
     设置关联图片
@@ -150,8 +150,8 @@ def set_attached_picture(images, attached_table, attached_id):
             )
 
 
-@current_app.task(
-    base=PeriodicTask, run_every=(crontab(minute=0, hour=8)), ignore_result=True, name='blog_daily.morning_message'
+@shared_task(
+    run_every=(crontab(minute=0, hour=8)), ignore_result=True, name='blog_daily.morning_message'
 )
 def morning_message():
     robot_send_alert(
@@ -160,8 +160,8 @@ def morning_message():
     )
 
 
-@current_app.task(
-    base=PeriodicTask, run_every=(crontab(minute=0, hour=23)), ignore_result=True, name='blog_daily.night_message'
+@shared_task(
+    run_every=(crontab(minute=0, hour=23)), ignore_result=True, name='blog_daily.night_message'
 )
 def night_message():
     robot_send_alert(
